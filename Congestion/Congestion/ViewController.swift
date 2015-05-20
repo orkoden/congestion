f70b884cb622584c11c915132609962fab294720
Robert Atkins <ratkins@fastmail.fm>
2015-05-20 11:06:33 +0200

    New Xcode project

---
+++ Congestion/Congestion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Congestion
+//
+//  Created by Robert Atkins on 20/05/2015.
+//  Copyright (c) 2015 Congestion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Congestion/CongestionTests/CongestionTests.swift
@@ -0,0 +1,36 @@
+//
+//  CongestionTests.swift
+//  CongestionTests
+//
+//  Created by Robert Atkins on 20/05/2015.
+//  Copyright (c) 2015 Congestion. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CongestionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
